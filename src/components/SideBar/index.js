431ffd1d01e7868a46416e60aeee70d1522d658c
Jason171096 <bboyace10@gmail.com>
2021-05-13 18:55:22 -0500

    SideBar Complete

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { SidebarContainer, Icon, CloseIcon, SidebarWrapper, SidebarMenu, SidebarLink, SidebarBtnWrap, SidebarRoute } from './SideBarElements'
 
 export const Sidebar = () => {
     return (
-        <div>
-            
-        </div>
+        <SidebarContainer>
+            <Icon>
+                <CloseIcon/>
+            </Icon>
+            <SidebarWrapper>
+                <SidebarMenu>
+                    <SidebarLink to='about'>About</SidebarLink>
+                    <SidebarLink to='discover'>Discover</SidebarLink>
+                    <SidebarLink to='services'>Services</SidebarLink>
+                    <SidebarLink to='signup'>Sign Up</SidebarLink>
+                </SidebarMenu>
+                <SidebarBtnWrap>
+                    <SidebarRoute to='/'>Sign In</SidebarRoute>
+                </SidebarBtnWrap>
+            </SidebarWrapper>
+        </SidebarContainer>
     )
 }
+
+export default Sidebar;
